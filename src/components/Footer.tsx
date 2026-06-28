53921ae15108bdbe00c30f2cc1639bf325867154
toshstandardmarket <chigoziemichaelihuoma@gmail.com>
2026-06-28 03:49:18 +0100

    Create Footer component with social links and ownership
    This component includes social media links and ownership information.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,61 @@
+function Footer(){
+
+
+return (
+
+<footer className="footer">
+
+
+<div className="socials">
+
+
+<a href="#">
+Telegram
+</a>
+
+
+<a href="#">
+YouTube
+</a>
+
+
+<a href="#">
+TikTok
+</a>
+
+
+<a href="#">
+WhatsApp
+</a>
+
+
+</div>
+
+
+
+
+<p className="owner">
+
+Owned by: Chigozie Michael Ihuoma
+
+</p>
+
+
+
+<p>
+
+© TOSH PREMIUM BOT
+
+</p>
+
+
+
+</footer>
+
+);
+
+
+}
+
+
+export default Footer;
